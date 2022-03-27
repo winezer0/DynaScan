3c07ea0f5fef04723d349dd19772a43d876cc659
WINEZERO <kissloo@foxmail.com>
2022-03-27 23:29:33 +0800

    添加工具logo

--- libs/InputParse.py
+++ libs/InputParse.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 import sys
 sys.dont_write_bytecode = True  # 设置不生成pyc文件
-
+from pyfiglet import Figlet
 import argparse
 # 获取版本号,并返回版本号字符串
 from setting import version
@@ -20,19 +20,20 @@ class ParserCmd(object):
         self.args = self.parser.parse_args().__dict__
 
     def my_parser(self):
-        example = """Examples:
-                          \r  python3 {shell_name} -u http://www.baidu.com
-                          \r  python3 {shell_name} -f target.txt
-                          \r  python3 {shell_name} -f target.txt -p http://127.0.0.1:8080
-                          \r  
-                          \r  其他控制细节参数请通过setting.py进行配置
-                          \r  
-                          \r  T00L Version: {version}
-                          \r  
-                          """
+        example = """
+                \rExamples:
+                \r  python3 {shell_name} -u http://www.baidu.com
+                \r  python3 {shell_name} -f target.txt
+                \r  python3 {shell_name} -f target.txt -p http://127.0.0.1:8080
+                \r    
+                \r  其他控制细节参数请通过setting.py进行配置
+                \r    
+                \r  T00L Version: {version}
+                \r  """
+        parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, add_help=True, description=Figlet().renderText("DynaScan"))
 
-        parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, add_help=True,)
         parser.epilog = example.format(shell_name=parser.prog, version=version)
+
         parser.add_argument("-u", dest="target", type=str, default=None, help="指定扫描目标URL, Example: http://www.baidu.com")
 
         parser.add_argument("-f", dest="target_file", type=str, default=None, help="指定扫描目标URL文件, Example: target.txt")
